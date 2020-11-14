ac8dc3c300efd12a2ca6ea6ac93f8091a24dc11e
Sergey Belesev <belesev@gmail.com>
2020-11-14 16:02:26 +0300

    problem 3 Longest Substring Without Repeating Characters

---
+++ Problem3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left_ptr = 0
+        right_ptr = 0
+        unique_symbols = dict()
+        best_length = 0
+
+        for symbol in s:
+            if symbol in unique_symbols:
+                # set left_ptr of the window to the previous+1 position
+                previous_entrance = unique_symbols[symbol]
+                left_ptr = previous_entrance + 1
+
+                # remove from dict of last entrances everything what is lefter than left_ptr
+                for x in unique_symbols.copy():
+                    if unique_symbols[x] < left_ptr:
+                        unique_symbols.pop(x, None)
+
+            # update where last entrance of current symbol is
+            unique_symbols[symbol] = right_ptr
+
+            if right_ptr - left_ptr + 1 > best_length:
+                best_length = right_ptr - left_ptr + 1
+                longest_substring_candidate = s[left_ptr:left_ptr+best_length]
+                print(longest_substring_candidate)
+
+            # move on right pointer
+            right_ptr += 1
+
+        return best_length
---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Problem3 import Solution
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    sol = Solution()
+    result = sol.lengthOfLongestSubstring("abcxxabcdee")
+    print()
+    print("Result is", result)
